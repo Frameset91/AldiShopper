0817727259742af2505c0afaaafe3841e09a3263
Frameset91 <frameset@arcor.de>
2013-03-30 02:35:47 +0100

    Initial stuff angelegt + Logo

--- src/de/aldi/shopper/MainActivity.java
+++ src/de/aldi/shopper/MainActivity.java
@@ -2,7 +2,9 @@ package de.aldi.shopper;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -20,4 +22,18 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    public void openNewcart(View view){
+    	Intent newcart = new Intent(this, Newcart.class);
+    	startActivity(newcart);
+    }
+    
+    public void openActivecart(View view){
+    	Intent activecart = new Intent(this, Activecart.class);
+    	startActivity(activecart);
+    }
+    
+    public void openOptions(View view){
+    	Intent options = new Intent(this, Options.class);
+    	startActivity(options);
+    }
 }
