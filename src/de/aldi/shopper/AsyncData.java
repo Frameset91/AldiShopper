3c67343e90df6e66fdf62ae069fef4b34a00247d
Frameset91 <frameset@arcor.de>
2013-04-06 17:51:37 +0200

    Onlineabfrage Speichern in Json-String

---
+++ src/de/aldi/shopper/AsyncData.java
@@ -0,0 +1,31 @@
+//package de.aldi.shopper;
+//
+//import android.app.ProgressDialog;
+//import android.content.Context;
+//import android.os.AsyncTask;
+//
+//public class AsyncData extends AsyncTask<Void, Void, Result> {
+//
+//	private ProgressDialog dlg;
+//	private Context ctx;
+//	
+//	public AsyncRequest(Context context) {
+//		ctx = context;
+//	}
+//	
+//	protected void onPreExecute() {
+//		dlg = new ProgressDialog(ctx);
+//		dlg.setMessage("Artikel werden abgerufen");
+//		dlg.show();
+//		super.onPreExecute();
+//	}
+//	
+//	protected void doInBackground() {
+//		
+//	}
+//	
+//	protected void onPostExecute() {
+//		
+//	}
+//
+//}
--- src/de/aldi/shopper/GetDataTest.java
+++ src/de/aldi/shopper/GetDataTest.java
@@ -1,15 +1,47 @@
 package de.aldi.shopper;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.util.Log;
 import android.view.Menu;
 
 public class GetDataTest extends Activity {
 
+	private Handler mHandler;
+	private ProgressDialog dialog;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_get_data_test);
+		
+		dialog = ProgressDialog.show(GetDataTest.this, "", "Hier Lädt ein Dings!", true);
+		mHandler = new Handler();
+		loadData.start();
+		
 	}
 
 	@Override
@@ -18,5 +50,134 @@ public class GetDataTest extends Activity {
 		getMenuInflater().inflate(R.menu.get_data_test, menu);
 		return true;
 	}
+	
+	private Thread loadData = new Thread() {
+		public void run() {
+			
+			try {
+				JSONArray jarr = getJsonArray("http://alexbusch.de/android/connect.php");
+				
+				for(int i = 0; i<jarr.length();i++){
+					
+					JSONObject tmpObj =  jarr.getJSONObject(i);
+					tmpObj.getString("productID");
+					tmpObj.getString("name");
+					tmpObj.getString("categoryID");
+					tmpObj.getString("cdesc");
+					tmpObj.getString("description");
+					tmpObj.getString("unit");
+					tmpObj.getString("price");
+					
+					
+					
+//					Log.i("Debug mich!!! "+i, tmpObj.getString("productID"));
+				}
+			
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+		mHandler.post(showUpdate);
+		
+			}
+		};
+		
+	private Runnable showUpdate = new Runnable() {
+		public void run() {
+		dialog.dismiss();
+			}
+		};	
 
+		/**
+		 * Lädt anhand einer URL ein JSON Objekt
+		 * 
+		 * @param URL
+		 *            URL als String die geladen werden soll
+		 * @return JSONObject oder NULL wenn es einen Fehler gibt
+		 */
+		private JSONArray getJsonArray(String URL) {
+			// Init
+			String 			JsonResponse 					= connect(URL, null);
+			JSONArray 	json									= null;
+			
+			try {
+				json = (new JSONArray(JsonResponse));	// Convert String to JSON (screws Ordering of JSON)
+			} catch (Exception e) {
+				//Log.e("##############################################", "UNIverse getJson Exception: "+URL);
+			}
+
+			return json;
+		}
+		
+		/**
+		 * Stelle eine HTTP Verbindung zu einer URL her und gibt den Inhalt zurück
+		 * 
+		 * @param url
+		 *            Die URL
+		 * @return
+		 */
+		public static String connect(String url, List<NameValuePair> postParams) {
+			DefaultHttpClient httpclient = new DefaultHttpClient();
+
+			HttpPost httpType = new HttpPost(url);
+
+			HttpResponse response;
+			try {
+				if(postParams != null)
+					httpType.setEntity(new UrlEncodedFormEntity(postParams));
+				
+				response = httpclient.execute(httpType);
+				HttpEntity entity = response.getEntity();
+				if (entity != null) {
+					InputStream instream = entity.getContent();
+					String result = convertStreamToString(instream);
+				
+					instream.close();
+					return result;
+				}
+			} catch (ClientProtocolException e) {
+				//Log.e("##############################################", "UNIverse exception2: "+url);
+			} catch (IOException e) {
+				//Log.e("##############################################", "UNIverse exception3: "+url);
+			}
+			return null;
+		}
+		
+		/**
+		 * Wandelt einen InputStream in einen String um. Diese Funktion versucht den
+		 * Stream !komplett! auszulesen. Daher kann es nur für Datei oder URLs
+		 * auslesen verwendet werden.
+		 * 
+		 * @param is
+		 * @return
+		 */
+		public static String convertStreamToString(InputStream is) {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+			StringBuilder sb = new StringBuilder();
+
+			String line = null;
+			try {
+				while ((line = reader.readLine()) != null) {
+					sb.append(line + "\n");
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					is.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return sb.toString();
+		}	// end method
+		
+		public boolean checkNetwork() {
+		    ConnectivityManager connectivityManager 
+		          = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		    NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+		    return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
+		}
 }
